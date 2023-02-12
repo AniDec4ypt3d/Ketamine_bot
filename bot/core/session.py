cc1978b5599f3b074523597eceb34d5df10019a5
ANIDEC4YP3D <102978058+AniDec4ypt3d@users.noreply.github.com>
2023-02-12 13:56:08 +0530

    Update session.py

--- bot/core/session.py
+++ bot/core/session.py
@@ -9,10 +9,7 @@ from .logger import log
 LOGS = logging.getLogger(__name__)
 
 
-# tgbot = TgClient(
-#     "tg", api_id=22617364, api_hash="c8670439ede9d5cdd354ec06df474698"
-# ).start(bot_token="5801605605:AAE4_dUVMoc7T2wme7lVTf2I_614PTiszkI")
-# loop=asyncio.get_event_loop()
+
 
 client = my(
     "tgv",
